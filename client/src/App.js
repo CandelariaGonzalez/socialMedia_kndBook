7240b78b39802c618635fa65f719bda00f2c6d31
Candelaria Gonzalez <m.candelariagonzalez.s@gmail.com>
2023-08-15 17:15:33 -0300

    Register, Login Pages, and Form

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./scenes/homePage";
 import LoginPage from "./scenes/loginPage";
 import ProfilePage from "./scenes/profilePage";
-// import { useMemo } from 'react';
-// import { useSelector } from 'react-redux';
-// import { CssBaseline, ThemeProvider } from '@mui/material';
-// import { createTheme } from "@mui/material";
-// import { themeSettings } from "./theme";
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { ThemeProvider, Container } from '@mui/material';
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 
 function App() {
-  // const mode = useSelector((state) => state.mode)
-  // const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const mode = useSelector((state) => state.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state)=> state.token));
+ 
   return (
 
     <div className="App">
       <BrowserRouter>
-        {/* <ThemeProvider theme={theme}> */}
-          {/* <CssBaseline /> */}
+        <ThemeProvider theme={theme}>
+          <Container>
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route path="/home" element={isAuth ? <HomePage /> : <Navigate to='/'/>} />
+            <Route path="/profile/:userId" element={isAuth ? <ProfilePage /> : <Navigate to='/'/>} />
           </Routes>
-        {/* </ThemeProvider> */}
+          </Container>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
